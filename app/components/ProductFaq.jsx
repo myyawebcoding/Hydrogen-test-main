893aca3fd0a009b55e8fe1236295fb679c04297e
こまゐ図考室 <komaizukoshitsu@gmail.com>
2023-11-07 20:27:26 +0900

    7th commit

--- app/components/ProductFaq.jsx
+++ app/components/ProductFaq.jsx
@@ -1,48 +1,42 @@
 import React, { useState } from "react";
 
-export default function FAQ({ key }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const items = [
-    { id: 1, question: "完成までにどのくらいかかりますか？", answer: "納期は通常約1.5ヶ月とさせていただいております。なお、予期せぬ事態により変更になる場合がありますのでご了承ください。" },
-    { id: 2, question: "似顔絵の衣装はどんなふうになるのですか？　結婚式の前撮りなどをしていないのですが...", answer: "ご注文から受け渡しまでのスケジュールは1.5ヶ月を予定しております。" },
-    { id: 3, question: "似顔絵の髪型はどんなふうになるのですか？　結婚式の前撮りなどをしていないのですが...", answer: "ご注文から受け渡しまでのスケジュールは1.5ヶ月を予定しております。" }
+export default function FAQ() {
+  const [openAnswer, setOpenAnswer] = useState({0: false});
+  const faqs = [
+    { question: "完成までにどのくらいかかりますか？", answer: "納期は通常約1.5ヶ月とさせていただいております。なお、予期せぬ事態により変更になる場合がありますのでご了承ください。" },
+    { question: "似顔絵の衣装はどんなふうになるのですか？　結婚式の前撮りなどをしていないのですが...", answer: "ご注文から受け渡しまでのスケジュールは1.5ヶ月を予定しております。" },
+    { question: "似顔絵の髪型はどんなふうになるのですか？　結婚式の前撮りなどをしていないのですが...", answer: "ご注文から受け渡しまでのスケジュールは1.5ヶ月を予定しております。" }
   ];
-  // const toggle = () => {
-  //   setIsOpen(!isOpen);
-  // };
-  const toggle = (props) => {
-    // setIsOpen(!isOpen);
-    setValue(props);
+
+  const handleOpenAnswer = (index) => {
+    setOpenAnswer((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index]
+    }));
+    console.log(openAnswer);
   }
 
   return (
     <div>
-      {items.map((item) => (
-        <div>
+      {faqs.map((faq, index) => (
+        <div
+          key={faq.question}
+          className="border-b border-[#E8E8E9]"
+        >
           <div
-            onClick={() => toggle(item.id)}
-          >{item.question}<span>{`${value === (item.id) ? '-' : '+'}`}</span></div>
+            className="py-2 text-sm"
+            onClick={() => handleOpenAnswer(index)}
+          >
+            {faq.question}<span>{`${openAnswer[index] ? '-' : '+'}`}</span>
+          </div>
           <div
-          className={`text-xs transition-all delay-150 duration-300 overflow-hidden ${value === (item.id) ? 'max-h-24' : 'max-h-0'
+          className={`text-xs transition-all delay-150 duration-300 overflow-hidden ${openAnswer[index] ? 'max-h-24' : 'max-h-0'
             }`}
           >
-            {item.answer}
+            {faq.answer}
           </div>
         </div>
       ))}
-      {/* <div
-        key={1}
-        onClick={getValue}
-      >
-        <div>完成までにどのくらいかかりますか？<span>{isOpen ? '-' : '+'}</span></div>
-        <div
-          className={`text-xs transition-all delay-150 duration-300 overflow-hidden ${isOpen ? 'max-h-24' : 'max-h-0'
-            }`}
-        >
-          納期は通常約1.5ヶ月とさせていただいております。なお、予期せぬ事態により変更になる場合がありますのでご了承ください。
-        </div>
-      </div> */}
     </div>
   );
 }
